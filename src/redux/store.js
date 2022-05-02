480550b27b604070737eeacb453a7b4b8613eedb
adriantims1 <79445916+adriantims1@users.noreply.github.com>
2022-05-01 22:22:54 -0400

    final release

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,6 @@ import { composeWithDevTools } from "redux-devtools-extension";
 import thunk from "redux-thunk";
 import { persistStore, persistReducer } from "redux-persist";
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
 import rootReducer from "./reducers";
 
 const persistConfig = {
