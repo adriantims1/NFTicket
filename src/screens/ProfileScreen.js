2e4ca801b0cb9d7cafb563dbca6a5de797f78e44
iamroshanpoudel <roshan.poudel@stonybrook.edu>
2022-05-04 17:15:09 -0400

    made changes to ios

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -2,15 +2,15 @@ import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { Dimensions, StyleSheet, TouchableOpacity } from "react-native";
 import {
-  Avatar,
-  HStack,
-  Box,
-  VStack,
-  Text,
-  IconButton,
-  Button,
-  FlatList,
-  Fab,
+	Avatar,
+	HStack,
+	Box,
+	VStack,
+	Text,
+	IconButton,
+	Button,
+	FlatList,
+	Fab,
 } from "native-base";
 import BigTicket from "../components/tickets/BigTicket";
 import EditProfileIcon from "../components/icons/EditProfileIcon";
@@ -22,147 +22,147 @@ import moment from "moment";
 const { height, width } = Dimensions.get("window");
 
 const ProfileScreen = ({ navigation, profile, ticket, event }) => {
-  const [tab, useTab] = useState(true); //false: history tab; true: active tab
+	const [tab, useTab] = useState(true); //false: history tab; true: active tab
 
-  const handleSignOut = () => {
-    auth
-      .signOut()
-      .then(() => {
-        navigation.replace("Login");
-      })
-      .catch((error) => alert(error.message));
-  };
+	const handleSignOut = () => {
+		auth
+			.signOut()
+			.then(() => {
+				navigation.replace("Login");
+			})
+			.catch((error) => alert(error.message));
+	};
 
-  return (
-    <SafeAreaView>
-      <Box style={styles.container} mt={4}>
-        {/* ----- Header ----- */}
-        <HStack justifyContent={"space-between"} alignItems="center">
-          <HStack alignItems={"center"}>
-            <Avatar
-              size="lg"
-              source={{
-                uri: profile.avatarURL,
-              }}
-            >
-              {`${profile.firstName[0]}${profile.lastName[0]}`}
-            </Avatar>
-            <VStack ml={4}>
-              <Text fontSize="xl" fontWeight={700}>
-                {`${profile.firstName} ${profile.lastName}`}
-              </Text>
-              <Text>{`@${profile.username}`}</Text>
-            </VStack>
-          </HStack>
-          <IconButton
-            variant={"unstyled"}
-            icon={<EditProfileIcon color="black" />}
-            color="transparent"
-            onPress={() => {
-              navigation.navigate("ProfileEdit");
-            }}
-          ></IconButton>
-        </HStack>
-        {/* ------------------ */}
-        {/* ----- Tab Navigator ----- */}
-        <HStack justifyContent={"space-evenly"} my={4}>
-          <Button
-            variant="unstyled"
-            _text={{ fontSize: "lg" }}
-            onPress={() => {
-              useTab(true);
-            }}
-            opacity={tab ? 1 : 0.25}
-          >
-            Active
-          </Button>
-          <Button
-            variant="unstyled"
-            _text={{ fontSize: "lg" }}
-            onPress={() => useTab(false)}
-            opacity={tab ? 0.25 : 1}
-          >
-            History
-          </Button>
-        </HStack>
-        {/* ------------------ */}
-        {/* ----- Display ----- */}
-        <Box alignItems={"center"} h="100%" flex={1}>
-          <FlatList
-            data={
-              tab
-                ? ticket.allTicket.filter((el) => !el.ticket.is_expired)
-                : ticket.allTicket.filter((el) => el.ticket.is_expired)
-            }
-            renderItem={({ item }) => {
-              const { title, date, time } = item;
-              const dateMoment = moment(
-                `${date} ${time}`,
-                "YYYY-MM-DD hh:mm:ss"
-              );
+	return (
+		<SafeAreaView>
+			<Box style={styles.container} mt={4}>
+				{/* ----- Header ----- */}
+				<HStack justifyContent={"space-between"} alignItems="center">
+					<HStack alignItems={"center"}>
+						<Avatar
+							size="lg"
+							source={{
+								uri: profile.avatarURL,
+							}}
+						>
+							{`${profile.firstName[0]}${profile.lastName[0]}`}
+						</Avatar>
+						<VStack ml={4}>
+							<Text fontSize="xl" fontWeight={700}>
+								{`${profile.firstName} ${profile.lastName}`}
+							</Text>
+							<Text>{`@${profile.username}`}</Text>
+						</VStack>
+					</HStack>
+					<IconButton
+						variant={"unstyled"}
+						icon={<EditProfileIcon color="black" />}
+						color="transparent"
+						onPress={() => {
+							navigation.navigate("ProfileEdit");
+						}}
+					></IconButton>
+				</HStack>
+				{/* ------------------ */}
+				{/* ----- Tab Navigator ----- */}
+				<HStack justifyContent={"space-evenly"} my={4}>
+					<Button
+						variant="unstyled"
+						_text={{ fontSize: "lg" }}
+						onPress={() => {
+							useTab(true);
+						}}
+						opacity={tab ? 1 : 0.25}
+					>
+						Active
+					</Button>
+					<Button
+						variant="unstyled"
+						_text={{ fontSize: "lg" }}
+						onPress={() => useTab(false)}
+						opacity={tab ? 0.25 : 1}
+					>
+						History
+					</Button>
+				</HStack>
+				{/* ------------------ */}
+				{/* ----- Display ----- */}
+				<Box alignItems={"center"} h="100%" flex={1}>
+					<FlatList
+						data={
+							tab
+								? ticket.allTicket.filter((el) => !el.ticket.is_expired)
+								: ticket.allTicket.filter((el) => el.ticket.is_expired)
+						}
+						renderItem={({ item }) => {
+							const { title, date, time } = item;
+							const dateMoment = moment(
+								`${date} ${time}`,
+								"YYYY-MM-DD hh:mm:ss"
+							);
 
-              const monthNames = [
-                "January",
-                "February",
-                "March",
-                "April",
-                "May",
-                "June",
-                "July",
-                "August",
-                "September",
-                "October",
-                "November",
-                "December",
-              ];
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    navigation.navigate("TicketDetail", { id: item.ticket.id });
-                  }}
-                >
-                  <BigTicket
-                    title={title}
-                    date={`${dateMoment.date()} ${
-                      monthNames[dateMoment.month()]
-                    } ${dateMoment.year()} - ${time}`}
-                  />
-                </TouchableOpacity>
-              );
-            }}
-            keyExtractor={(item, index) => item.ticket.id}
-            showsVerticalScrollIndicator={false}
-            contentContainerStyle={{
-              flexGrow: 1,
-            }}
-          ></FlatList>
-        </Box>
+							const monthNames = [
+								"January",
+								"February",
+								"March",
+								"April",
+								"May",
+								"June",
+								"July",
+								"August",
+								"September",
+								"October",
+								"November",
+								"December",
+							];
+							return (
+								<TouchableOpacity
+									onPress={() => {
+										navigation.navigate("TicketDetail", { id: item.ticket.id });
+									}}
+								>
+									<BigTicket
+										title={title}
+										date={`${dateMoment.date()} ${
+											monthNames[dateMoment.month()]
+										} ${dateMoment.year()} - ${time}`}
+									/>
+								</TouchableOpacity>
+							);
+						}}
+						keyExtractor={(item, index) => item.ticket.id}
+						showsVerticalScrollIndicator={false}
+						contentContainerStyle={{
+							flexGrow: 1,
+						}}
+					></FlatList>
+				</Box>
 
-        {/* ------------------ */}
-        <Fab
-          renderInPortal={false}
-          shadow={2}
-          size="md"
-          icon={<LogoutIcon color="black" />}
-          onPress={() => handleSignOut()}
-        />
-      </Box>
-    </SafeAreaView>
-  );
+				{/* ------------------ */}
+				<Fab
+					renderInPortal={false}
+					shadow={2}
+					size="md"
+					icon={<LogoutIcon color="black" />}
+					onPress={() => handleSignOut()}
+				/>
+			</Box>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: "90%",
-    alignSelf: "center",
-    height: height * 0.9,
-  },
+	container: {
+		width: "90%",
+		alignSelf: "center",
+		height: height * 0.84,
+	},
 });
 
 const mapStateToProps = ({ profile, ticket, event }) => ({
-  profile,
-  ticket,
-  event,
+	profile,
+	ticket,
+	event,
 });
 
 const mapDispatchToProps = {};
--- src/screens/TicketDetailScreen.js
+++ src/screens/TicketDetailScreen.js
@@ -19,145 +19,145 @@ const { height, width } = Dimensions.get("window");
 import { connect } from "react-redux";
 
 const TicketDetailScreen = ({ navigation, ticket }) => {
-  const [nftId, setNftId] = useState("");
-  const [eventName, setEventName] = useState("");
-  const [location, setLocation] = useState("");
-  const [date, setDate] = useState("");
-  const [price, setPrice] = useState("");
-  const [description, setDescription] = useState("");
-  const [onSale, setOnSale] = useState(false);
-  const [isExpired, setIsExpired] = useState(false);
-  const [event, setEvent] = useState(null);
-  const [owner, setOwner] = useState("");
-  const [valid, setValid] = useState("");
+	const [nftId, setNftId] = useState("");
+	const [eventName, setEventName] = useState("");
+	const [location, setLocation] = useState("");
+	const [date, setDate] = useState("");
+	const [price, setPrice] = useState("");
+	const [description, setDescription] = useState("");
+	const [onSale, setOnSale] = useState(false);
+	const [isExpired, setIsExpired] = useState(false);
+	const [event, setEvent] = useState(null);
+	const [owner, setOwner] = useState("");
+	const [valid, setValid] = useState("");
 
-  useEffect(() => {
-    const theTicket = ticket.allTicket.find(
-      (el) => el.ticket.id === navigation.getParam("id", "")
-    );
-    const {
-      ticket_nft_id,
-      title,
-      description,
-      street_address,
-      city,
-      zipcode,
-      state,
-      date,
-      time,
-    } = theTicket;
-    const { price, on_sale, is_expired, event, owner } = theTicket.ticket;
+	useEffect(() => {
+		const theTicket = ticket.allTicket.find(
+			(el) => el.ticket.id === navigation.getParam("id", "")
+		);
+		const {
+			ticket_nft_id,
+			title,
+			description,
+			street_address,
+			city,
+			zipcode,
+			state,
+			date,
+			time,
+		} = theTicket;
+		const { price, on_sale, is_expired, event, owner } = theTicket.ticket;
 
-    setOwner(owner);
-    setNftId(ticket_nft_id);
-    setEventName(title);
-    setLocation(`${street_address}, ${city} ${state} ${zipcode}`);
-    setPrice(price);
-    setDescription(description);
-    setOnSale(on_sale);
-    setEvent(event);
-    setIsExpired(is_expired);
+		setOwner(owner);
+		setNftId(ticket_nft_id);
+		setEventName(title);
+		setLocation(`${street_address}, ${city} ${state} ${zipcode}`);
+		setPrice(price);
+		setDescription(description);
+		setOnSale(on_sale);
+		setEvent(event);
+		setIsExpired(is_expired);
 
-    setValid(date);
-    const dateMoment = moment(`${date} ${time}`, "YYYY-MM-DD hh:mm:ss");
-    const monthNames = [
-      "January",
-      "February",
-      "March",
-      "April",
-      "May",
-      "June",
-      "July",
-      "August",
-      "September",
-      "October",
-      "November",
-      "December",
-    ];
-    setDate(
-      `${dateMoment.date()} ${
-        monthNames[dateMoment.month()]
-      } ${dateMoment.year()} - ${time}`
-    );
-  }, []);
+		setValid(date);
+		const dateMoment = moment(`${date} ${time}`, "YYYY-MM-DD hh:mm:ss");
+		const monthNames = [
+			"January",
+			"February",
+			"March",
+			"April",
+			"May",
+			"June",
+			"July",
+			"August",
+			"September",
+			"October",
+			"November",
+			"December",
+		];
+		setDate(
+			`${dateMoment.date()} ${
+				monthNames[dateMoment.month()]
+			} ${dateMoment.year()} - ${time}`
+		);
+	}, []);
 
-  return (
-    <SafeAreaView>
-      <Box style={styles.container} mt={4}>
-        {/* ----- Navigator ----- */}
-        <HStack alignItems="center" justifyContent="space-between">
-          <IconButton
-            icon={<BackIcon color="black" />}
-            onPress={() => {
-              navigation.goBack();
-            }}
-            variant="unstyled"
-          ></IconButton>
-          <Text fontWeight={700} fontSize="lg">
-            ID {nftId}
-          </Text>
-        </HStack>
-        {/* ------------------ */}
-        {/* ----- Ticket ----- */}
-        <DetailTicket valid={valid} owner={owner} nftId={nftId} />
-        {/* ------------------ */}
-        {/* ----- Detail ----- */}
-        <ScrollView showsVerticalScrollIndicator={false} mt={4}>
-          <Text fontSize="2xl" fontWeight={700} mb={2}>
-            {eventName}
-          </Text>
-          <HStack mb={2}>
-            <PinIcon color="black" />
-            <Text ml={2} fontWeight={700} fontSize="md">
-              {location}
-            </Text>
-          </HStack>
-          <HStack mb={2}>
-            <CalendarIcon color="black" />
-            <Text ml={2} fontWeight={700} fontSize="md">
-              {date}
-            </Text>
-          </HStack>
-          <HStack mb={2}>
-            <AlgorandIcon color="black" />
-            <Text ml={2} fontWeight={700} fontSize="md">
-              {price}
-            </Text>
-          </HStack>
-          <Text fontWeight={700} fontSize="md">
-            {description}
-          </Text>
-        </ScrollView>
-        {/* ------------------ */}
-        <ActionButton
-          text="Resell"
-          width="100%"
-          onPress={() => {
-            navigation.navigate("ResellTicket", {
-              price,
-              onSale,
-              id: navigation.getParam("id", ""),
-              nftId,
-              isExpired,
-              event,
-            });
-          }}
-        />
-      </Box>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView>
+			<Box style={styles.container} mt={4}>
+				{/* ----- Navigator ----- */}
+				<HStack alignItems="center" justifyContent="space-between">
+					<IconButton
+						icon={<BackIcon color="black" />}
+						onPress={() => {
+							navigation.goBack();
+						}}
+						variant="unstyled"
+					></IconButton>
+					<Text fontWeight={700} fontSize="lg">
+						ID {nftId}
+					</Text>
+				</HStack>
+				{/* ------------------ */}
+				{/* ----- Ticket ----- */}
+				<DetailTicket valid={valid} owner={owner} nftId={nftId} />
+				{/* ------------------ */}
+				{/* ----- Detail ----- */}
+				<ScrollView showsVerticalScrollIndicator={false} mt={4}>
+					<Text fontSize="2xl" fontWeight={700} mb={2}>
+						{eventName}
+					</Text>
+					<HStack mb={2}>
+						<PinIcon color="black" />
+						<Text ml={2} fontWeight={700} fontSize="md">
+							{location}
+						</Text>
+					</HStack>
+					<HStack mb={2}>
+						<CalendarIcon color="black" />
+						<Text ml={2} fontWeight={700} fontSize="md">
+							{date}
+						</Text>
+					</HStack>
+					<HStack mb={2}>
+						<AlgorandIcon color="black" />
+						<Text ml={2} fontWeight={700} fontSize="md">
+							{price}
+						</Text>
+					</HStack>
+					<Text fontWeight={700} fontSize="md">
+						{description}
+					</Text>
+				</ScrollView>
+				{/* ------------------ */}
+				<ActionButton
+					text="Resell"
+					width="100%"
+					onPress={() => {
+						navigation.navigate("ResellTicket", {
+							price,
+							onSale,
+							id: navigation.getParam("id", ""),
+							nftId,
+							isExpired,
+							event,
+						});
+					}}
+				/>
+			</Box>
+		</SafeAreaView>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: "90%",
-    alignSelf: "center",
-    height: height * 0.95,
-  },
+	container: {
+		width: "90%",
+		alignSelf: "center",
+		height: height * 0.89,
+	},
 });
 const mapStateToProps = ({ ticket, profile }) => ({
-  ticket,
-  profile,
+	ticket,
+	profile,
 });
 
 const mapDispatchToProps = {};
